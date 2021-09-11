c48f5edf50f1fcec87b1ccbfbcb0c3ef184d71e9
dunk <fake@invalid.fake>
2021-09-11 19:09:02 -0400

    Maybe it'll work this time?

--- guix/installer.scm
+++ guix/installer.scm
@@ -12,29 +12,7 @@
   #:use-module (nongnu packages linux)
   #:use-module (nongnu system linux-initrd)
   #:use-module (guix)
-  #:use-module (guix channels)
   #:export (installation-os-dunkmania))
-
-(define %guix-channels
-  (scheme-file
-   "channels.scm"
-   #~(cons* (channel
-             (name 'nonguix)
-             (url "https://gitlab.com/nonguix/nonguix")
-             (introduction
-              (make-channel-introduction
-               "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-               (openpgp-fingerprint
-                "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-            (channel
-             (name 'flat)
-             (url "https://github.com/flatwhatson/guix-channel.git")
-             (introduction
-              (make-channel-introduction
-               "33f86a4b48205c0dc19d7c036c85393f0766f806"
-               (openpgp-fingerprint
-                "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490")))))
-            %default-channels))
 
 (define %xorg-libinput-config
   "Section \"InputClass\"
@@ -130,7 +108,6 @@ EndSection
             (service openssh-service-type)
             (service tor-service-type)
             (service cups-service-type)
-            (extra-special-file "/etc/guix/channels.scm" %guix-channels)
             (simple-service 'channel-file etc-service-type
                             (list `("channels.scm" ,(local-file "channels.scm"))))
             (extra-special-file "/usr/bin/env"
