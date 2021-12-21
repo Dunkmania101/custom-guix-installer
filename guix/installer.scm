6dc0447ac5ea4bc8f180453e8441714a4f106f43
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2021-12-21 00:16:17 -0500

    Hopefully fix the fport_read error during installation.

--- guix/installer.scm
+++ guix/installer.scm
@@ -1,19 +1,6 @@
 (use-modules (gnu) (gnu services) (gnu system) (gnu system install) (guix) (guix channels) (nongnu packages linux))
 (use-package-modules linux)
 
-(define %guix-channels
-  (scheme-file
-    "channels.scm"
-    #~(cons* (channel
-               (name 'nonguix)
-               (url "https://gitlab.com/nonguix/nonguix")
-               (introduction
-                 (make-channel-introduction
-                   "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-                   (openpgp-fingerprint
-                     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-             %default-channels)))
-
 (operating-system
   (inherit installation-os)
   (kernel linux)
@@ -40,8 +27,6 @@
   (services
     (append
       (list 
-        (extra-special-file "/etc/guix/channels.scm" %guix-channels)
-        (extra-special-file "/mnt/etc/guix/channels.scm" %guix-channels)
         (extra-special-file "/usr/bin/env"
                             (file-append coreutils "/bin/env")))
       (modify-services (operating-system-user-services installation-os)
