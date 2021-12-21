eba106a1a35d133173052b9439d0a9cb8e767a51
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2021-12-20 20:01:25 -0500

    Gymnastics

--- guix/installer.scm
+++ guix/installer.scm
@@ -3,16 +3,16 @@
 
 (define %guix-channels
   (scheme-file
-   "channels.scm"
-   #~(cons* (channel
-             (name 'nonguix)
-             (url "https://gitlab.com/nonguix/nonguix")
-             (introduction
-              (make-channel-introduction
-               "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-               (openpgp-fingerprint
-                "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-            %default-channels)))
+    "channels.scm"
+    #~(cons* (channel
+               (name 'nonguix)
+               (url "https://gitlab.com/nonguix/nonguix")
+               (introduction
+                 (make-channel-introduction
+                   "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+                   (openpgp-fingerprint
+                     "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+             %default-channels)))
 
 (operating-system
   (inherit installation-os)
@@ -21,37 +21,39 @@
   (firmware (list linux-firmware))
 
   (packages
-   (append (list
-            (specification->package "emacs-no-x-toolkit")
-            (specification->package "bash")
-            (specification->package "usb-modeswitch")
-            (specification->package "network-manager")
-            (specification->package "mobile-broadband-provider-info")
-            (specification->package "git")
-            (specification->package "chrony")
-            (specification->package "curl")
-            (specification->package "stow")
-            (specification->package "vim")
-            (specification->package "nss-certs")
-            (specification->package "ncurses"))
-           (operating-system-packages installation-os)))
+    (append (list
+              (specification->package "emacs-no-x-toolkit")
+              (specification->package "bash")
+              (specification->package "usb-modeswitch")
+              (specification->package "network-manager")
+              (specification->package "mobile-broadband-provider-info")
+              (specification->package "git")
+              (specification->package "chrony")
+              (specification->package "curl")
+              (specification->package "stow")
+              (specification->package "vim")
+              (specification->package "nss-certs")
+              (specification->package "ncurses"))
+            (operating-system-packages installation-os)))
 
   (services
-   (append
-    (list 
-      (guix-configuration
-        (substitute-urls
-          (append (list "https://substitutes.nonguix.org")
-                  %default-substitute-urls))
-        (authorized-keys
-          (append (list (plain-file "non-guix.pub"
-                                     "(public-key 
-                                        (ecc 
-                                          (curve Ed25519)
-                                          (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
-                  %default-authorized-guix-keys)))
-      (extra-special-file "/etc/guix/channels.scm" %guix-channels)
-      (extra-special-file "/mnt/etc/guix/channels.scm" %guix-channels)
-      (extra-special-file "/usr/bin/env"
-                          (file-append coreutils "/bin/env")))
-     (operating-system-user-services installation-os))))
+    (append
+      (list 
+        (extra-special-file "/etc/guix/channels.scm" %guix-channels)
+        (extra-special-file "/mnt/etc/guix/channels.scm" %guix-channels)
+        (extra-special-file "/usr/bin/env"
+                            (file-append coreutils "/bin/env")))
+      (modify-services (operating-system-user-services installation-os)
+                       (guix-service-type config => (guix-configuration
+                                                      (inherit config)
+                                                      (substitute-urls
+                                                        (append (list "https://substitutes.nonguix.org")
+                                                                %default-substitute-urls))
+                                                      (authorized-keys
+                                                        (append (list (plain-file "non-guix.pub"
+                                                                                  "(public-key 
+                                                                                     (ecc 
+                                                                                       (curve Ed25519)
+                                                                                       (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+                                                                %default-authorized-guix-keys))))))))
+
