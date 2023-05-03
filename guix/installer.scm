d28a3d581bb4d1b0fa73290a809de0664521aea2
Dunkmania101 <50186904+Dunkmania101@users.noreply.github.com>
2023-05-03 16:48:59 -0400

    Disable `firefox` package because it makes the ISO too big

--- guix/installer.scm
+++ guix/installer.scm
@@ -42,7 +42,7 @@
               ;  (synopsis "Browsh is a fully-modern text-based browser")
               ;  (description "Browsh is a fully-modern text-based browser. It renders anything that a modern browser can; HTML5, CSS3, JS, video and even WebGL. Its main purpose is to be run on a remote server and accessed via SSH/Mosh or the in-browser HTML service in order to significantly reduce bandwidth and thus both increase browsing speeds and decrease bandwidth costs.")
               ;  (license license:lgpl2.1))
-              (specification->package "firefox")
+              ;(specification->package "firefox")
               (specification->package "links")
               (specification->package "lynx")
               (specification->package "emacs-no-x-toolkit")
